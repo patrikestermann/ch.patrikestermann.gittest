c5f038b37172e9532135e6add1d69261bcf0861a
unknown <Patrik.Estermann@mgb.ch>
2015-07-14 16:46:15 +0200

    Initial commit

---
+++ src/main/java/ch/patrikestermann/GitTest.java
@@ -0,0 +1,12 @@
+package ch.patrikestermann;
+/**
+ * Created by pesterma on 14.07.2015.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+          System.out.println("Hello world");
+
+    }
+}
