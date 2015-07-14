204f1b5e9ab0994df7feff0de3d987b95b798efc
patrikestermann <patrik.estermann@bluewin.ch>
2015-07-14 17:02:52 +0200

    Ein Commit

--- src/main/java/ch/patrikestermann/GitTest.java
+++ src/main/java/ch/patrikestermann/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
 
           System.out.println("Hello world");
           System.out.println("Nochmals Hallo");
+          System.out.println("Glücklich ist das nicht");
 
     }
 }
